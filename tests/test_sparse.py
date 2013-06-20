9b8510e498a5c1e3cc71a3869e53500cf3b388b2
Michael Kane <kaneplusplus@gmail.com>
2013-06-20 12:42:39 -0400

    Added support for sparsity in the console application.

--- tests/test_sparse.py
+++ tests/test_sparse.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 import scipy.sparse as sp
+from irlb import *
 
 def main():
   m   = 20000   # Number of rows
@@ -13,8 +14,11 @@ def main():
 
   v   = np.random.rand(nnz)
   i   = np.random.randint(0,high=m,size=nnz)
-  j   = np.random.randint(0,high=m,size=nnz)
+  j   = np.random.randint(0,high=n,size=nnz)
   A   = sp.csc_matrix((v,(i,j)),shape=(m,n))
   X = irlb(A,nu,tol=0.000001)
   # A gut check measurement of absolute decomposition error:
   print("TSVD: ||AV - US||_F = %f" % np.linalg.norm(A.dot(sp.csr_matrix(X[2])).todense().A - X[0]*X[1],"fro"))
+
+  S = sp.linalg.svds(A, nu)
+  abserr = np.max(np.abs(X[1]-S[1][0:nu]))
